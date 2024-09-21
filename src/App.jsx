8d9d6930c70200d2bece0dde2e6849cbfc312c49
dev-khairpal <133212101+dev-khairpal@users.noreply.github.com>
2024-09-22 00:15:22 +0530

    Defined Routes and made pages for routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,25 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup"
+import Offers from "./pages/Offers";
+import ForgotPassword from "./pages/ForgotPassword"
+
 function App() {
   return(
-    <div className="text-3xl">Hello</div>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/sign-in" element={<Signin />} />
+          <Route path="/sign-up" element={<Signup />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/offers" element={<Offers />} />
+        </Routes>
+      </Router>
+    </>
   )
 }
 
